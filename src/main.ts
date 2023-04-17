3b57828c3333b8e9f0fb549bfb6f0c19944fafa8
cygnan <email@cygnan.com>
2023-04-17 22:16:54 +0900

    Add code draft (successfully made bot online)

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+// const { Client, Intents } = require('discord.js');
+import {Client, GatewayIntentBits, TextChannel} from "discord.js";
+const { token, channelId } = require('../config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    if (oldState.member !== null) {
+        const channel = oldState.member.guild.channels.cache.get(channelId);
+
+        if (oldState.channelId === null && newState.channelId !== null) {
+            if (oldState.member !== null) {
+                (channel as TextChannel).send(`**参加** ${oldState.member.user.tag}さんが入室しました。`);
+                return;
+            }
+        } else if (oldState.channelId !== null && newState.channelId === null) {
+            if (newState.member !== null) {
+                (channel as TextChannel).send(`**退出** ${newState.member.user.tag}さんが退出しました。`);
+                return;
+            }
+        }
+    }
+});
+
+client.login(token);
