8db87e6d20b2b0f267cc978d2d372550a6e6e567
cygnan <email@cygnan.com>
2023-04-17 22:49:04 +0900

    Change display text

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // const { Client, Intents } = require('discord.js');
 import {Client, GatewayIntentBits, TextChannel} from "discord.js";
-const { token, channelId } = require('../config.json');
+const {token, channelId} = require('../config.json');
 
 const client = new Client({
     intents: [
@@ -15,12 +15,14 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 
         if (oldState.channelId === null && newState.channelId !== null) {
             if (oldState.member !== null) {
-                (channel as TextChannel).send(`[**参加**] ${oldState.member.displayName} さんが入室しました。`);
+                (channel as TextChannel).send(
+                    `**${oldState.member.displayName}** さんが入室しました。`
+                );
                 return;
             }
         } else if (oldState.channelId !== null && newState.channelId === null) {
             if (newState.member !== null) {
-                (channel as TextChannel).send(`[**退出**] ${newState.member.displayName} さんが退出しました。`);
+                (channel as TextChannel).send(`**${newState.member.displayName}** さんが退出しました。`);
                 return;
             }
         }
